0aa7cb610e22817aa829261452ddd5d9d4f097bb
Pablo Santiago Blum de Aguiar <scorphus@gmail.com>
2016-07-13 17:48:14 -0300

    fixup! fixup! wip

--- init.fish
+++ init.fish
@@ -1,7 +1,4 @@
-if test -z "$PYENV_ROOT"
-  setenv PYENV_ROOT $HOME/.pyenv
-end
-setenv PATH $PYENV_ROOT/shims $PATH
+setenv PATH $HOME/.pyenv/shims $PATH
 setenv PYENV_SHELL fish
-command pyenv rehash 2>/dev/null
 setenv PYENV_VIRTUALENV_DISABLE_PROMPT 1
+command pyenv rehash 2>/dev/null
